4b8cb78a42eeb33931224e7950ec5c61a8c7bf4a
Pedro Silva <pedrinhopedrao135@gmail.com>
2026-06-23 19:55:59 -0300

    Implementação da Listagem de Pokemons Favoritos

--- screens/FavoritosScreen.js
+++ screens/FavoritosScreen.js
@@ -1,34 +1,97 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet
+} from "react-native";
+
 import { db } from "../services/firebaseConfig";
-import { View, Text, TouchableOpacity } from "react-native";
+
+import {
+  collection,
+  getDocs
+} from "firebase/firestore";
 
 export default function FavoritosScreen({ navigation }) {
 
-  console.log("TESTE FIREBASE");
-  console.log(db);
+  const [favoritos, setFavoritos] = useState([]);
+
+  useEffect(() => {
+    carregarFavoritos();
+  }, []);
+
+  async function carregarFavoritos() {
+    try {
+
+      const querySnapshot = await getDocs(
+        collection(db, "favoritos")
+      );
+
+      const lista = [];
+
+      querySnapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+
+      setFavoritos(lista);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Tela de Favoritos</Text>
+    <View style={styles.container}>
 
-      <TouchableOpacity
-        style={{
-          backgroundColor: "red",
-          padding: 10,
-          borderRadius: 8,
-          marginTop: 20,
-        }}
-        onPress={() => navigation.goBack()}
-      >
-        <Text style={{ color: "white" }}>
-          Voltar
-        </Text>
-      </TouchableOpacity>
+      <Text style={styles.titulo}>
+        Favoritos
+      </Text>
+
+      <FlatList
+        data={favoritos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.nome}>
+              {item.nome}
+            </Text>
+          </View>
+        )}
+      />
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0066ff",
+    padding: 20,
+  },
+
+  titulo: {
+    color: "white",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+
+  card: {
+    backgroundColor: "#164ea1",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+
+  nome: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
